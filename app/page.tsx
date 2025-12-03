10de453379dab8f9061af064a7f7cd1ac074f164
SJM <simjung418@gmail.com>
2025-12-03 10:07:55 +0900

    sectionTitle 잠시 빼기

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
-import SectionTitle from "@/components/sectionTitle";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
-const title: string = metadata.title;
-
-console.log(title);
-
 export default function Home() {
   return (
     <>
-      <SectionTitle param={title} />
-      <div>멀보냐</div>
+      <div>Home</div>
     </>
   );
 }
