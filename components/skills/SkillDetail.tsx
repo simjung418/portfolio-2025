8ebbcd3a9876b03a7567e9bc8d63e43fa141d5ca
SJM <simjung418@gmail.com>
2026-03-09 12:52:59 +0900

    chore: 상대경로 절대경로로 변경

--- components/skills/SkillDetail.tsx
+++ components/skills/SkillDetail.tsx
@@ -1,6 +1,6 @@
 import { SkillData, SkillKey } from "@/lib/portfolio/skills";
 import clsx from "clsx";
-import { ctaBtnClassMap } from "../button/ctaBtnClassMap";
+import { ctaBtnClassMap } from "@/lib/portfolio/classNameMap";
 import Link from "next/link";
 
 type Props = {
@@ -11,9 +11,9 @@ type Props = {
 export const SkillDetail = ({ card, name }: Props) => {
   return (
     <>
-      <h3 className="font-bold md:mb-6 md:text-4xl">{card.name}</h3>
-      <h4 className="pl-2 font-bold md:text-2xl">주요 활용 기능</h4>
-      <ul className="my-4 flex flex-col gap-3 pl-5 font-medium md:text-xl">
+      <h3 className="md:mb-6 md:text-4xl font-bold">{card.name}</h3>
+      <h4 className="md:text-2xl pl-2 font-bold">주요 활용 기능</h4>
+      <ul className="md:text-xl flex flex-col gap-3 pl-5 my-4 font-medium">
         {card.features.map((f) => (
           <li key={f}>{f}</li>
         ))}
@@ -21,10 +21,8 @@ export const SkillDetail = ({ card, name }: Props) => {
 
       {card.projects ? (
         <>
-          <h4 className="mt-6 pl-2 font-bold md:text-2xl">
-            관련 프로젝트 / 커리어
-          </h4>
-          <ul className="my-4 flex flex-col gap-4 pl-5">
+          <h4 className="md:text-2xl pl-2 mt-6 font-bold">관련 프로젝트 / 커리어</h4>
+          <ul className="flex flex-col gap-4 pl-5 my-4">
             {card.projects.map((p) => (
               <li key={p.name}>
                 <Link
@@ -32,16 +30,12 @@ export const SkillDetail = ({ card, name }: Props) => {
                   className={clsx(
                     "w-full min-w-20 flex-1 justify-center rounded-md py-3 md:w-fit md:flex-none md:px-6 md:py-5 md:text-xl",
                     ctaBtnClassMap.base,
-                    ctaBtnClassMap.active,
+                    ctaBtnClassMap.active
                   )}
                 >
                   <div>
-                    <strong className="mb-4 block text-xl font-bold">
-                      {p.name}
-                    </strong>
-                    <span className="font-medium text-neutral-700">
-                      {p.summary}
-                    </span>
+                    <strong className="block mb-4 text-xl font-bold">{p.name}</strong>
+                    <span className="text-neutral-700 font-medium">{p.summary}</span>
                   </div>
                 </Link>
               </li>
--- components/skills/SkillSections.tsx
+++ components/skills/SkillSections.tsx
@@ -1,11 +1,8 @@
 import { SkillKey } from "@/lib/portfolio/skills";
 import { SkillList } from "./SkillList";
-import {
-  SKILL_SECTION_KEYS,
-  SKILL_SECTIONS,
-} from "@/app/(portfolio)/skills/skill.content";
+import { SKILL_SECTION_KEYS, SKILL_SECTIONS } from "@/app/(portfolio)/skills/skill.content";
 import clsx from "clsx";
-import { ctaBtnClassMap } from "../button/ctaBtnClassMap";
+import { ctaBtnClassMap } from "@/lib/portfolio/classNameMap";
 
 type Props = {
   activeSkill: SkillKey;
@@ -14,25 +11,21 @@ type Props = {
 
 export const SkillSections = ({ activeSkill, onSelect }: Props) => {
   return (
-    <ul className="z-10 sticky top-26 mb-4 flex items-center gap-4 overflow-x-auto md:pb-2 whitespace-nowrap md:mb-7">
+    <ul className="sticky top-26 z-10 mb-4 flex items-center gap-4 overflow-x-auto whitespace-nowrap md:mb-7 md:pb-2">
       {SKILL_SECTION_KEYS.map((sectionKey) => {
         const section = SKILL_SECTIONS[sectionKey];
         return (
           <li key={sectionKey} className="flex items-center gap-4">
             <h3
               className={clsx(
-                `w-full min-w-20 justify-center rounded-full px-4 py-2 text-center font-bold! whitespace-nowrap border-0`,
+                `w-full min-w-20 justify-center rounded-full border-0 px-4 py-2 text-center font-bold! whitespace-nowrap`,
                 ctaBtnClassMap.base,
-                ctaBtnClassMap.inactive,
+                ctaBtnClassMap.inactive
               )}
             >
               {section.name}
             </h3>
-            <SkillList
-              section={sectionKey}
-              activeSkill={activeSkill}
-              onSelect={onSelect}
-            ></SkillList>
+            <SkillList section={sectionKey} activeSkill={activeSkill} onSelect={onSelect}></SkillList>
           </li>
         );
       })}
