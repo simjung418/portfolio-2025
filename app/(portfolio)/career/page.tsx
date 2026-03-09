022bf8568ee9b307ed6cd598d64041a42ce7b245
SJM <simjung418@gmail.com>
2026-03-09 13:04:47 +0900

    docs: 경력 총 기간 추가

--- app/(portfolio)/career/page.tsx
+++ app/(portfolio)/career/page.tsx
@@ -10,8 +10,8 @@ export default function CareerPage() {
   const { selected, onSelect, trigger } = useCareer();
   return (
     <>
-      <h3 className="text-3xl font-bold text-neutral-700">LEXCODE Inc.</h3>
-      <ul className="mt-2 flex flex-wrap items-center gap-x-2 gap-y-1 md:gap-x-4">
+      <h3 className="text-neutral-700 text-3xl font-bold">LEXCODE Inc.</h3>
+      <ul className="gap-x-2 gap-y-1 md:gap-x-4 flex flex-wrap items-center mt-2">
         <li className="flex items-center gap-2">
           <span className={badgeClass}>직급</span>
           <p className="text-lg font-semibold">대리</p>
@@ -22,7 +22,7 @@ export default function CareerPage() {
         </li>
         <li className="flex items-center gap-2">
           <span className={badgeClass}>재직기간</span>
-          <p className="text-lg font-semibold">2020.07 ~ 2025.08</p>
+          <p className="text-lg font-semibold">2020.07 ~ 2025.08 (5년 2개월)</p>
         </li>
       </ul>
       <p className="mt-4 mb-10 text-base/6.5 font-medium break-keep text-neutral-600 md:text-xl md:leading-normal">
@@ -30,13 +30,13 @@ export default function CareerPage() {
         <br />각 항목은 문제 정의부터 해결 과정, 그리고 결과까지의 흐름으로 구성했습니다.
       </p>
 
-      <div className="my-4 h-px w-full bg-neutral-300 md:my-8"></div>
+      <div className="bg-neutral-300 md:my-8 w-full h-px my-4"></div>
 
       <section className="relative">
         <CareerList list={CAREER_LIST} onSelect={onSelect} cards={CAREER_CARDS} selected={selected} />
         {CAREER_LIST.map((name, index) => (
           <div key={name} className="scroll-mt-24">
-            {index !== 0 && <div className="my-4 h-px w-full bg-neutral-300 md:my-8"></div>}
+            {index !== 0 && <div className="bg-neutral-300 md:my-8 w-full h-px my-4"></div>}
             <CareerCardDetail
               triggerRef={(el) => {
                 trigger.current[name] = el;
