b7efac0820d2631234991408775529f8e21d9702
SJM <simjung418@gmail.com>
2026-05-16 16:27:28 +0900

    chore: change phone number for global

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="md:font-semibold font-medium text-sm md:text-neutral-400 text-neutral-300 mt-7">
       <ul className="flex md:flex-row flex-col md:gap-3 gap-1 justify-end items-end">
-        <li>010-3917-6880</li>
+        <li>+82 10-3917-6880</li>
         <li className="h-4 w-px bg-neutral-300 md:block hidden"></li>
         <li>simjung418@naver.com</li>
         <li className="h-4 w-px bg-neutral-300 md:block hidden"></li>
